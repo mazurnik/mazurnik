3b62e54d888ba3f7c5fa75e07be639376ef667e9
Mikalai <mazurnikv@gmail.com>
2020-04-29 11:13:36 +0300

    add slick slider

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.slider__inner').slick({
+        speed: 300,
+        // adaptiveHeight: true,
+        // prevArrow: 'button type="button" class="slick-prev"></button',
+        // nextArrow: 'button type="button" class="slick-next"><img src="../img/icons/next.svg"></button',
+    });
+});
