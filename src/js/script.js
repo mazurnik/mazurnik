15d6bef4651d4ee789f2f535e7548a14dd593c23
Mikalai <mazurnikv@gmail.com>
2020-05-03 13:55:55 +0300

    пофиксил слайдер

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $('.slider__inner').slick({
         speed: 300,
-        autoplay: true,
-        // prevArrow: 'button type="button" class="slick-prev"><img src="img/icons/prev.svg" alt="slide"></button',
-        // nextArrow: 'button type="button" class="slick-next"><img src="../img/icons/next.svg"></button',
+        // autoplay: true,
+        prevArrow: '<img class ="slick-prev" src = "../img/icons/prev.svg" alt="slide">',
+        nextArrow: '<img class="slick-next" src="../img/icons/next.svg">',
     });
 });
