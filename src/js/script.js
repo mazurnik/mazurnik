46b7db1e5cfadb52adb47061ebe965bc2ab39b16
Mikalai <mazurnikv@gmail.com>
2020-05-08 12:08:58 +0300

    добавил скрипты для модал окон но не все работает, доделать

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,7 @@ $(document).ready(function () {
   $(".slider__inner").slick({
     speed: 700,
     // autoplay: true,
-    prevArrow:
-      '<img class ="slick-prev" src = "../img/icons/prev.svg" alt="slide">',
+    prevArrow: '<img class ="slick-prev" src = "../img/icons/prev.svg" alt="slide">',
     nextArrow: '<img class="slick-next" src="../img/icons/next.svg">',
   });
 
@@ -33,4 +32,20 @@ $(document).ready(function () {
       $(".catalog__item-list").eq(i).toggleClass("active");
     });
   });
-});
+  //формы
+  $('[data-modal=consultation]').on('click', function () {
+    $('.shadow, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function () {
+    $('.shadow, #consultation, #order, #thanks').fadeOut('slow');
+  });
+  // $('.button__mini').on('click', function () {
+  //   $('.shadow, #order').fadeIn('slow');
+  // });
+  $('.button__mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal_subtitle').text($('.catalog__item-subtitle').eq(i).text());
+      $('.shadow, #order').fadeIn('slow');
+    })
+  });
+})
